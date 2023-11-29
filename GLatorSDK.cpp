ef046156ef151bd94963093a3439aa9fd9b052e6
Afaq Ahmad <134938313+Honestopedia-TECHAFAQ@users.noreply.github.com>
2023-11-29 15:33:15 +0500

    Create GLatorSDK.cpp

---
+++ GLatorSDK.cpp
@@ -0,0 +1,22 @@
+#include "GLatorSDK.h"
+
+PF_Err GLatorSDK_Initialize()
+{
+   
+    return PF_Err_NONE;
+}
+
+void GLatorSDK_BeginRender()
+{
+
+}
+
+void GLatorSDK_SetLayerAlpha(float alpha)
+{
+
+}
+
+void GLatorSDK_EndRender()
+{
+
+}
