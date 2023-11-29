89f83488ed12c2c2bed00ed077ed712d97321bed
Afaq Ahmad <134938313+Honestopedia-TECHAFAQ@users.noreply.github.com>
2023-11-29 15:31:32 +0500

    Update MainPlugin.h

--- MainPlugin.h
+++ MainPlugin.h
@@ -4,7 +4,7 @@
 #include <AE_Macros.h>
 #include <AE_EffectCB.h>
 #include <AE_EffectUI.h>
-#include "GLatorSDK.h" 
+#include "GLatorSDK.h"
 
 static PF_Err GlobalSetup(PF_GlobalData *gData, PF_AppUID appUID, PF_ProgID appID);
 static PF_Err ParamsSetup(PF_InData *in_data, PF_OutData *out_data, PF_ParamDef *params[]);
@@ -13,10 +13,9 @@ static PF_Err RenderTriangle(const PF_InData *in_data, const PF_OutData *out_dat
 static PF_Err DrawWhiteTriangle(const PF_InData *in_data, double *vertex1, double *vertex2, double *vertex3);
 
 DllExport PF_Err PluginMain(
-  PF_Cmd cmd,
-  PF_InData *in_data,
-  PF_OutData *out_data,
-  PF_ParamDef *params[],
-  PF_LayerDef *output,
-  void *extra);
-
+    PF_Cmd cmd,
+    PF_InData *in_data,
+    PF_OutData *out_data,
+    PF_ParamDef *params[],
+    PF_LayerDef *output,
+    void *extra);
